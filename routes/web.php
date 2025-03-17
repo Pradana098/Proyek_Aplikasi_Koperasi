b93df996551ecfa9c00fa5794e639dc0cbda32bc
Pradana098 <pradanadimas534@gmai.com>
2025-03-17 12:34:54 +0700

    login update

--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
-
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.post');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::middleware(['role:pengurus'])->group(function () {
-    Route::get('/dashboard/pengurus', function () {
-        return view('dashboard.pengurus');
-    })->name('dashboard.pengurus');
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard/pengawas', [DashboardController::class, 'pengawas'])->name('dashboard.pengawas');
-    Route::get('/dashboard/pengurus', [DashboardController::class, 'pengurus'])->name('dashboard.pengurus');
-});
-
-Route::middleware(['role:pengawas'])->group(function () {
-    Route::get('/dashboard/pengawas', function () {
-        return view('dashboard.pengawas');
-    })->name('dashboard.pengawas');
-});
-
 
 Route::get('/', function () {
-    return view('beranda');
+    return view('welcome');
 });
 
-Route::get('/pegawai', function () {
-    return view('pegawai');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/anggota', function () {
-    return view('anggota');
-});
-
-Route::get('/simpanan', function () {
-    return view('simpanan');
-});
-
-Route::get('/angsuran', function () {
-    return view('angsuran');
-});
-
-Route::get('/laporan', function () {
-    return view('laporan');
-});
-
-Route::get('/setting', function () {
-    return view('setting');
-});
+require __DIR__.'/auth.php';
